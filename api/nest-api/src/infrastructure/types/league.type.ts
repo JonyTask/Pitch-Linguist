f2cc702090aec9ef37329d89cc24242f6e6626cb
JonyTask <yuusu.hyb@gmail.com>
2025-02-21 12:01:57 +0900

    add : league.type.ts

---
+++ api/nest-api/src/infrastructure/types/league.type.ts
@@ -0,0 +1,10 @@
+export type LeagueType = 'J1League' | 'PremierLeague' | 'LaLiga' | 'serieA' | 'Bundesliga' | 'Ligue1';
+
+export const NumEachLeague = {
+    'J1League': 1,
+    'PremierLeague': 2,
+    'LaLiga': 3,
+    'serieA': 4,
+    'Bundesliga': 5,
+    'Ligue1': 6
+}
