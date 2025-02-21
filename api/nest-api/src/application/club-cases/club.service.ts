cfa3746055e052c878a423d82a96b773066e1969
JonyTask <yuusu.hyb@gmail.com>
2025-02-21 12:01:12 +0900

    feat : fetchClubsByLeague API

--- api/nest-api/src/application/club-cases/club.service.ts
+++ api/nest-api/src/application/club-cases/club.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/domain/prisma/prisma.service";
-import { Club } from '@prisma/client';
+import { LeagueType } from "src/infrastructure/types/league.type";
+import { NumEachLeague } from "src/infrastructure/types/league.type";
+import { SerializeBigInt } from "src/infrastructure/utils/serialize";
 
 @Injectable({})
 export class ClubService {
@@ -10,9 +12,22 @@ export class ClubService {
         try {
             const clubs = await this.prisma.club.findMany();
             // BigInt を文字列に変換してから返す
-            return JSON.parse(JSON.stringify(clubs, (key, value) =>
-                typeof value === 'bigint' ? value.toString() : value
-            ));
+            return SerializeBigInt.serialize(clubs);
+        } catch (error) {
+            console.error('Error fetching clubs:', error);
+            throw error;
+        }
+    }
+
+    async fetchClubsByLeague(league: LeagueType) {
+        try {
+            const leagueNum: number = NumEachLeague[`${league}`];
+            const clubs = await this.prisma.club.findMany({
+                where: {
+                    league_id: leagueNum
+                }
+            });
+            return SerializeBigInt.serialize(clubs);
         } catch (error) {
             console.error('Error fetching clubs:', error);
             throw error;
