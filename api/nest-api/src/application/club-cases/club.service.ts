80c13b492e744d3a2a1e53c904fd44dd545108c9
JonyTask <yuusu.hyb@gmail.com>
2025-02-21 13:30:31 +0900

    feat : format response -- dto

--- api/nest-api/src/application/club-cases/club.service.ts
+++ api/nest-api/src/application/club-cases/club.service.ts
@@ -3,6 +3,8 @@ import { PrismaService } from "src/domain/prisma/prisma.service";
 import { LeagueType } from "src/infrastructure/types/league.type";
 import { NumEachLeague } from "src/infrastructure/types/league.type";
 import { SerializeBigInt } from "src/infrastructure/utils/serialize";
+import { ClubDto } from "./dto/club.dto";
+import { plainToInstance } from "class-transformer";
 
 @Injectable({})
 export class ClubService {
@@ -11,8 +13,9 @@ export class ClubService {
     async listUpAllClubs(){
         try {
             const clubs = await this.prisma.club.findMany();
+            const dtoClubs = clubs.map(club => plainToInstance( ClubDto, club ));
             // BigInt を文字列に変換してから返す
-            return SerializeBigInt.serialize(clubs);
+            return SerializeBigInt.serialize(dtoClubs);
         } catch (error) {
             console.error('Error fetching clubs:', error);
             throw error;
@@ -27,7 +30,8 @@ export class ClubService {
                     league_id: leagueNum
                 }
             });
-            return SerializeBigInt.serialize(clubs);
+            const dtoClubs = clubs.map(club => plainToInstance(ClubDto, club));
+            return SerializeBigInt.serialize(dtoClubs);
         } catch (error) {
             console.error('Error fetching clubs:', error);
             throw error;
