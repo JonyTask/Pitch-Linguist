ba2b64d5a953d065f10ae196ebcdcce56c61c5ae
JonyTask <marv.gh.jony@gmail.com>
2025-04-05 11:47:59 +0900

    fix : extend expire -- access token

--- api/nest-api/src/application/auth-cases/auth.module.ts
+++ api/nest-api/src/application/auth-cases/auth.module.ts
@@ -15,7 +15,7 @@ import { LocalStrategy } from "./strategy/local.strategy";
         JwtModule.register({
             global: true,
             secret: jwtConstants.secret,
-            signOptions: { expiresIn: '60s' },
+            signOptions: { expiresIn: '1h' },
         }),
     ],
     providers: [AuthService, JWTStrategy, LocalStrategy],
