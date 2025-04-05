cfb2033bf80ff92c764cc472f771cbd98f185575
JonyTask <marv.gh.jony@gmail.com>
2025-04-05 23:40:53 +0900

    refactoring : remove duplicated logic & enable shared -- toggleStockState

--- api/nest-api/src/application/stock-cases/stock.module.ts
+++ api/nest-api/src/application/stock-cases/stock.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { PrismaModule } from "src/infrastructure/prisma/prisma.module";
 import { StockController } from "./stock.controller";
 import { StockService } from "./stock.service";
+import { DomainModule } from "src/domain/domain-service/domain.module";
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, DomainModule],
     controllers: [StockController],
     providers: [StockService],
     exports: [StockService]
--- api/nest-api/src/application/stock-cases/stock.service.ts
+++ api/nest-api/src/application/stock-cases/stock.service.ts
@@ -1,43 +1,17 @@
 import { Injectable, Request } from "@nestjs/common";
-import { PrismaService } from "src/infrastructure/prisma/prisma.service";
+import { ToggleEvent } from "src/domain/domain-service/toggle.event";
 
 @Injectable({})
 export class StockService {
-    constructor(private readonly prisma: PrismaService) { }
+    constructor( private readonly toggleEvent: ToggleEvent
+    ) {}
+
+    private readonly insertDB: string = 'stock';
     
     async toggleStockState(@Request() req, insert_match_review_id: bigint) {
         try {
             const userId = req.user.userId;
-            const existingLike = await this.prisma.stock.findUnique({
-                where: {
-                    // Prismaのモデルでstamper_idとmatch_review_idを複合主キーまたはユニーク制約にしている前提です
-                    unique_stocker_match_review: {
-                        stocker_id: userId,
-                        match_review_id: insert_match_review_id
-                    },
-                },
-            });
-            if (existingLike) {
-                // レコードが存在していれば削除する（いいねの解除）
-                await this.prisma.stock.delete({
-                    where: {
-                        unique_stocker_match_review: {
-                            stocker_id: userId,
-                            match_review_id: insert_match_review_id
-                        },
-                    },
-                });
-                return { message: 'Stock removed' };
-            } else {
-                // レコードが存在しなければ新規作成する（いいねする）
-                await this.prisma.stock.create({
-                    data: {
-                        stocker_id: userId,
-                        match_review_id: insert_match_review_id
-                    },
-                });
-                return { message: 'Stock added' };
-            }
+            return this.toggleEvent.checkExists(this.insertDB, userId, insert_match_review_id);
         } catch (error) {
             console.error('toggle stock state :', error);
             throw error;
